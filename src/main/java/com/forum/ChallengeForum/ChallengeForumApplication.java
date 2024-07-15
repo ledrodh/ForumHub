36771fd0916e424ae6dd4b22a86ac6748f2431fa
ledrodh <ledrodh@protonmail.com>
2024-07-15 12:26:36 -0300

    First commit

---
+++ src/main/java/com/forum/ChallengeForum/ChallengeForumApplication.java
@@ -0,0 +1,13 @@
+package com.forum.ChallengeForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forum/ChallengeForum/ChallengeForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.ChallengeForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
